a3104cd0b9f2460d36fe5b8d3024c838fbc4e81b
Nicolas (laptop) <ponchon.nicolas@gmail.com>
2026-04-10 14:07:48 +0200

    build: apply manifest to deploy guestbook app

--- scripts/setup-cluster.sh
+++ scripts/setup-cluster.sh
@@ -122,6 +122,12 @@ else
 
 fi
 
+#~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=#
+#                   Deploy apps                    #
+#~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=#
+
+kubectl apply -f argocd/home-lab-app.yaml -n argocd
+
 #   You should delete the argocd-initial-admin-secret from the Argo CD namespace once you changed the password.
 #   The secret serves no other purpose than to store the initially generated password in clear and can safely be deleted at any time.
 #   It will be re-created on demand by Argo CD if a new admin password must be re-generated.
